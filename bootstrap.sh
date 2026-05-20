168a1c278719718a4bc012b20c0648e343f43f70
ТвоÑIvan <ivanqqqe@gmail.com>
2026-05-20 22:02:54 +0200

    solution

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+kind create cluster --config cluster.yml &&
+
+kubectl create ns todoapp &&
+kubectl create ns mysql &&
+
+kubectl apply -f .infrastructure/pv.yml -n todoapp &&
+kubectl apply -f .infrastructure/pvc.yml -n todoapp &&
+kubectl apply -f .infrastructure/confgiMap.yml -n todoapp &&
+kubectl apply -f .infrastructure/secret.yml -n todoapp &&
+kubectl apply -f .infrastructure/deployment.yml -n todoapp &&
+
+kubectl apply -f .infrastructure/headless_service.yml -n mysql &&
+kubectl apply -f .infrastructure/mysql_secret.yml -n mysql &&
+kubectl apply -f .infrastructure/statefulSet.yml -n mysql
